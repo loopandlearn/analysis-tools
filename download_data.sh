176b9ee91d964989e1d214a352d9c753a9471e69
marionbarker <marionbarker@earthlink.net>
2023-06-15 22:26:29 -0700

    add argument for query_name

--- download_data.sh
+++ download_data.sh
@@ -8,6 +8,8 @@ if [ "$#" -lt 3 ]; then
     echo "start is date and time in utc format i.e. 2023-06-12T13:52:19Z"
     echo "duration is seconds from start, i.e. 3600 for an hour"
     echo "[api-key] is optional, use if the site requires it"
+    echo "[output_file] is optional, if not provided, output goes to terminal"
+    echo "[query_name] is optional, if not provided, devicestatus is used"
     echo "Pipe the result from the execution to a csv file in order to import to excel"
     exit 1
 fi
@@ -17,7 +19,12 @@ url="$1"
 start="$2"
 duration="$3"
 api_key="$4"
-output_json="$5"
+output_file="$5"
+query_name="$6"
+
+if [ -z "$query_name" ]; then
+    query_name="devicestatus"
+fi
 
 # Calculate end timestamp
 end=$(date -u -v+${duration}S -j -f "%Y-%m-%dT%H:%M:%SZ" "${start}" "+%Y-%m-%dT%H:%M:%SZ")
@@ -25,14 +32,15 @@ end=$(date -u -v+${duration}S -j -f "%Y-%m-%dT%H:%M:%SZ" "${start}" "+%Y-%m-%dT%
 # Make API request
 if [ -n "$api_key" ]; then
     API_SECRET=$(echo -n "$api_key" | shasum | awk '{print $1}')
-    response=$(curl -s -H "api-secret: ${API_SECRET}" "${url}/api/v1/devicestatus.json?count=1000&find%5Bcreated_at%5D%5B%24gte%5D=${start}&find%5Bcreated_at%5D%5B%24lte%5D=${end}")
+    echo "${url}/api/v1/$query_name.json"
+    response=$(curl -s -H "api-secret: ${API_SECRET}" "${url}/api/v1/$query_name.json?count=1000&find%5Bcreated_at%5D%5B%24gte%5D=${start}&find%5Bcreated_at%5D%5B%24lte%5D=${end}")
 else
-    response=$(curl -s "${url}/api/v1/devicestatus.json?count=1000&find%5Bcreated_at%5D%5B%24gte%5D=${start}&find%5Bcreated_at%5D%5B%24lte%5D=${end}")
+    response=$(curl -s "${url}/api/v1/$query_name.json?count=1000&find%5Bcreated_at%5D%5B%24gte%5D=${start}&find%5Bcreated_at%5D%5B%24lte%5D=${end}")
 fi
 
 # Output to file or Parse JSON and print results
-if [ -n "$output_json" ]; then
-    echo $response > $output_json
+if [ -n "$output_file" ]; then
+    echo $response > $output_file
 else
     echo $response | jq -r '.[] | "\(.created_at),\(.loop.iob.iob)"'
 fi
